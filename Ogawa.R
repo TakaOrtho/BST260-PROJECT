6f33952fa79917c8df9da980cde550abedde4548
Takahisa OGAWA <togawa@hsph.harvard.edu>
2019-11-21 21:21:09 -0500

    modified

--- Ogawa.R
+++ Ogawa.R
@@ -4,10 +4,27 @@ library(haven)
 library(tidyverse)
 library(readr)
 data <- read_csv("severeinjury.csv")
-data %>%   rename('Final Narrative' = 'narra')
 data <- data %>%   rename('narra' = 'Final Narrative')
-head(data)
+
+dist <- read_csv("EucDistHp.csv")
+dist <- dist %>%   rename('Zip' = 'ZIPCODE')
+
+cens <- read_csv("ACS_16_5YR_S1811_with_ann.csv")
+cens <- cens %>%   rename('Zip' = 'GEO.id2')
+
+cens <- cens[-1,]
+cens$Zip <- as.numeric(cens$Zip)
+
+hpno <- read_csv("HpNumPerZip.csv")
+hpno <- hpno %>%   rename('Zip' = 'ZIPCODE')
+
 names(data)
+
+#merge
+data <- left_join(data,dist,by="Zip")
+data <- left_join(data,cens,by="Zip")
+data <- left_join(data,hpno,by="Zip")
+head(data)
 
 #NLP
 text <- data %>%  unnest_tokens(word, narra)
@@ -33,6 +50,6 @@ wctext <- text %>% group_by(word) %>% mutate(Freq = n())
 wctext%>% select(word,Freq)%>% 
   distinct(word, .keep_all = TRUE)%>%
   arrange(desc(Freq)) %>% 
-  head(100)%>%
+  head(4)%>%
     wordcloud2()
 
