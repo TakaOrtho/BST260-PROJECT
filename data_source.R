24ea4446523de401c74363a64300205f96b132c2
Keiko Kunitoki <54902138+Keikokunitoki@users.noreply.github.com>
2019-12-05 16:48:50 -0500

    Update data_source.R

--- data_source.R
+++ data_source.R
@@ -1,12 +1,13 @@
 library(tidyverse)
 library(readr)
-data <- read_delim("injurydata.csv",";", escape_double = FALSE, trim_ws = TRUE)
+dataz <- read_delim("injurydata.csv",";", escape_double = FALSE, trim_ws = TRUE)
 
 #filter hospitarisation
-dataa <- data %>% filter(HOSPITALIZED<2)
+dataa <- dataz %>% filter(HOSPITALIZED<2)
 datas <- dataa %>% filter(HOSPITALIZED>-1)
 #filter
 datad <- datas %>% filter(AMPUTATION<2)
 dataf <- datad %>% filter(AMPUTATION>-1)
 dataq <- dataf %>% filter(NEAR_FID>0)
-datag <- dataq %>% filter(NEAR_DIST<10^6)
+data <- dataq %>% filter(NEAR_DIST<10^5)
+
